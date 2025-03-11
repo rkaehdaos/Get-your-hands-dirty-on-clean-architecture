52d3089937de6da429836b1c4802660d345e0abe
Kai Ahn <13996827+rkaehdaos@users.noreply.github.com>
2025-03-11 14:33:27 +0900

    Sourcery 의견 반영 개선 - 반환 값1
    컨트롤러
    - 스프링은 컨트롤러 메서드 내에서 예외가 발생하지 않으면 자동으로 200을 반환
    - `@RestController`로 뷰 리졸브 과정은 생략 됨
    - `SendMoneyCommand`의 `sendMoney()`는 `boolean`을 반환했지만
       컨트롤러에서 이 값을 사용하지 않고 void를 반환
    
    해당 값을 이용하도록 수정
    
    **Ticket**
    Github: #1
    
    Signed-off-by: Kai Ahn <13996827+rkaehdaos@users.noreply.github.com>

--- src/main/java/dev/haja/buckpal/account/adapter/in/web/SendMoneyController.java
+++ src/main/java/dev/haja/buckpal/account/adapter/in/web/SendMoneyController.java
@@ -5,6 +5,7 @@ import dev.haja.buckpal.account.application.port.in.SendMoneyUseCase;
 import dev.haja.buckpal.account.domain.Account.AccountId;
 import dev.haja.buckpal.account.domain.Money;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,11 +17,17 @@ class SendMoneyController {
     private final SendMoneyUseCase sendMoneyUseCase;
 
     @PostMapping(path = "/accounts/send")
-    void sendMoney(@RequestBody SendMoneyReqDto dto) {
-        SendMoneyCommand sendMoneyCommand = new SendMoneyCommand(
+    ResponseEntity<Void> sendMoney(@RequestBody SendMoneyReqDto dto) {
+        SendMoneyCommand command = new SendMoneyCommand(
                 new AccountId(dto.sourceAccountId()),
                 new AccountId(dto.targetAccountId()),
                 Money.of(dto.amount()));
-        sendMoneyUseCase.sendMoney(sendMoneyCommand);
+        boolean success = sendMoneyUseCase.sendMoney(command);
+        
+        if (!success) {
+            return ResponseEntity.badRequest().build();
+        }
+        
+        return ResponseEntity.ok().build();
     }
 }
