e969df02c39313f0f471e59082c7157769fd5939
Kai Ahn <13996827+rkaehdaos@users.noreply.github.com>
2025-04-09 17:30:06 +0900

    refactor: return을 if에서 빼내라
    - 인텔리제이 제안
    - 반환문을 여럿 사용하지 마라.
    Ticket
    - GitHub Issue: #7
    
    Signed-off-by: Kai Ahn <13996827+rkaehdaos@users.noreply.github.com>

--- src/main/kotlin/dev/haja/java2kotlin/Legs.kt
+++ src/main/kotlin/dev/haja/java2kotlin/Legs.kt
@@ -2,13 +2,13 @@ package dev.haja.java2kotlin
 
 import java.time.Duration
 
-
 fun longestLegOver(
     legs: List<Leg>,
     duration: Duration
 ): Leg? {
     val longestLeg: Leg? = legs.maxByOrNull(Leg::plannedDuration)
-    if (longestLeg != null && longestLeg.plannedDuration > duration)
-        return longestLeg
-    return null
+    return if (longestLeg != null && longestLeg.plannedDuration > duration)
+        longestLeg
+    else
+        null
 }
