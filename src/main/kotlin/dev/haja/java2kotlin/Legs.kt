a7721a4fbef119f893a7ff4f7daab90831c07d6b
Kai Ahn <13996827+rkaehdaos@users.noreply.github.com>
2025-04-09 17:01:53 +0900

    refactor: Leg파라미터를 수신객체로 변환
    Ticket
    - GitHub Issue: #7
    
    Signed-off-by: Kai Ahn <13996827+rkaehdaos@users.noreply.github.com>

--- src/main/kotlin/dev/haja/java2kotlin/Legs.kt
+++ src/main/kotlin/dev/haja/java2kotlin/Legs.kt
@@ -3,20 +3,19 @@ package dev.haja.java2kotlin
 import java.time.Duration
 
 
-    fun longestLegOver(
-        legs: List<Leg>,
-        duration: Duration
-    ): Leg? {
-        var result: Leg? = null
-        for (leg in legs) {
-            if (isLongerThan(leg, duration))
-                if (result == null ||
-                    isLongerThan(leg, result.plannedDuration)
-                )
-                    result = leg
-        }
-        return result
+fun longestLegOver(
+    legs: List<Leg>,
+    duration: Duration
+): Leg? {
+    var result: Leg? = null
+    for (leg in legs) {
+        if (leg.isLongerThan(duration))
+            if (result == null ||
+                leg.isLongerThan(result.plannedDuration))
+                result = leg
     }
+    return result
+}
 
-private fun isLongerThan(leg: Leg, duration: Duration) =
-    leg.plannedDuration.compareTo(duration) > 0
+private fun Leg.isLongerThan(duration: Duration) =
+    plannedDuration.compareTo(duration) > 0
