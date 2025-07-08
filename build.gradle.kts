734e04dd46013603b748ee602a963557e66b69ef
Kai Ahn <154768497+kai-ahn-businesson@users.noreply.github.com>
2025-07-08 10:49:07 +0900

    #18 hibernate version up
    Signed-off-by: Kai Ahn <13996827+rkaehdaos@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.hibernate.orm") version "6.6.13.Final"
+    id("org.hibernate.orm") version "6.6.18.Final"
     id("org.graalvm.buildtools.native") version "0.10.6"
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
