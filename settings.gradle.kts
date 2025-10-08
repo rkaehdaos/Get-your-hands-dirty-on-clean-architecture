d6fd5d86411ca531efb2dabe00b7243dacb56e40
Kai Ahn <kai.ahn@businesson.co.kr>
2025-10-08 18:31:44 +0900

    🔥remove(deps): version은 프로퍼티
    무조건 프로퍼티에 버전
    
    Signed-off-by: Kai Ahn <kai.ahn@businesson.co.kr>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,11 +2,11 @@ rootProject.name = "Get-your-hands-dirty-on-clean-architecture"
 
 pluginManagement {
     // 버전 변수 정의
-    val springBootVersion = providers.gradleProperty("springBootVersion").orNull ?: "3.5.6"
-    val hibernateVersion = providers.gradleProperty("hibernateVersion").orNull ?: "7.1.1.Final"
-    val kotlinVersion = providers.gradleProperty("kotlinVersion").orNull ?: "2.2.20"
-    val dependencyManageVer = providers.gradleProperty("dependencyManageVer").orNull ?: "1.1.7"
-    val nativeBuildVersion = providers.gradleProperty("nativeBuildVersion").orNull ?: "0.11.0"
+    val springBootVersion = providers.gradleProperty("springBootVersion")
+    val hibernateVersion = providers.gradleProperty("hibernateVersion")
+    val kotlinVersion = providers.gradleProperty("kotlinVersion")
+    val dependencyManageVer = providers.gradleProperty("dependencyManageVer")
+    val nativeBuildVersion = providers.gradleProperty("nativeBuildVersion")
 
 
     // ✅ 플러그인 저장소 지정 (필수!)
